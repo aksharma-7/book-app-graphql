d43bf54fab0d727a9e9e04b2709246adc524f58c
aksharma-7 <52443790+aksharma-7@users.noreply.github.com>
2021-06-12 17:45:17 +0530

    added booklist component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import BookList from './components/BookList';
 
 const App = () => {
   return (
     <div id='main'>
       <h1>Reading List</h1>
+      <BookList />
     </div>
   );
 };
